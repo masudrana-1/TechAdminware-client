83c6519d68e386fd66ba187c601138886799446c
masudrana-1 <masudrana58855@gmail.com>
2022-12-22 13:05:33 +0600

    project full polished

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -15,7 +15,7 @@ const NavBar = () => {
 
     return (
         <div>
-            <div className="navbar bg-base-100">
+            <div className="navbar bg-base-100 mb-10 pb-6 pt-3 shadow-lg shadow-cyan-500/50">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
